d58fe1425aba9e4ab00e161b4e75ac7db2bb1e5c
FURU-RINA <s2132125lx@s.chibakoudai.jp>
2025-01-10 19:25:16 +0900

    魚追加

--- place.js
+++ place.js
@@ -2,9 +2,17 @@ module.exports = [
   {
     name: "東京湾",
     type: "tokyo",
+    spring:"春",
+    summer:"夏",
+    autumn:"秋",
+    winter:"冬",
   },
   {
     name: "沖縄",
     type: "okinawa",
+    spring:"春",
+    summer:"夏",
+    autumn:"秋",
+    winter:"冬",
   },
 ];
